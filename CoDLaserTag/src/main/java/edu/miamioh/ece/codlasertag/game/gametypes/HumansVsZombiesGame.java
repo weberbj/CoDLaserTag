92b2ba39651650397d6325f135e47f7924ba9c92
rogerskw <rogerskw@miamioh.edu>
2014-02-14 14:25:31 -0500

    Moved game logic to HumansVsZombiesGame class and began work on issue #9

--- CoDLaserTag/src/main/java/edu/miamioh/ece/codlasertag/game/gametypes/HumansVsZombiesGame.java
+++ CoDLaserTag/src/main/java/edu/miamioh/ece/codlasertag/game/gametypes/HumansVsZombiesGame.java
@@ -7,12 +7,21 @@
 package edu.miamioh.ece.codlasertag.game.gametypes;
 
 import edu.miamioh.ece.codlasertag.game.Game;
+import edu.miamioh.ece.codlasertag.game.Team;
 
 /**
  *
  * @author kylerogers
  */
 public class HumansVsZombiesGame extends Game {
+    
+    public static final String ZOMBIES = "zombies";
+    public static final String HUMANS = "humans";
+    
+    public HumansVsZombiesGame()    {
+        this.teams.put(ZOMBIES, new Team(ZOMBIES));
+        this.teams.put(HUMANS, new Team(HUMANS));
+    }
 
     @Override
     protected void updateGame() {
